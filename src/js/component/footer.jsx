7117d6697d1c5a135019831446d6d2cdd51f090d
Dali <dalirod90@gmail.com>
2022-11-24 15:23:44 +0000

    second

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Footer = () => {
     return (
 
-        <div class="container-fluid" > 
-        <div class="card-body footer footer-expand-lg bg-dark row p-5">
+        <div className="container-fluid" > 
+        <div className="card-body footer footer-expand-lg bg-dark row p-5">
         
-        <p class="card-title text-white  d-flex justify-content-center">Copyright your website 2022</p>
+        <p className="card-title text-white  d-flex justify-content-center">Copyright your website 2022</p>
         
       </div>
       </div> 
